0b4f6fd6b72b6000376e7cd62d0c0c59e51e43d8
Jayasuryaa <jayasuryaa1998@gmail.com>
2021-10-26 11:08:50 -0500

    Added code to open_fs, blockWriter and initfs

--- FileSystem.c
+++ FileSystem.c
@@ -31,6 +31,7 @@ unsigned int modtime;
 } inode_type;
 
 superblock_type superblock;
+int fd;
 
 int open_fs(char *file_name){
     int fd = open(file_name, O_RDWR, 0600);
@@ -47,5 +48,40 @@ int open_fs(char *file_name){
         return 1;
 
     }
+}
+
+void blockWriter(int blockNumber, void * buffer, int size){
+
+    lseek(fd, blockNumber*BLOCK_SIZE, SEEK_SET);
+    write(fd, buffer, size);
+    
+}
+
+void initfs(int n1, int n2){
+
+    char fillerBlock[BLOCK_SIZE] = {0};
+    fd = open("foo.txt", O_RDWR | O_CREAT, 0600);
+    // printf("%d \n", fd);
+    blockWriter(n1 - 1, fillerBlock, BLOCK_SIZE);
+
+    superblock.nfree = 5;
+
+    blockWriter(1, &superblock, sizeof(superblock));
+
+}
+
+int main(){
+    int n1 = 10;
+    int n2 = 3;
+    superblock_type temp;
+
+    initfs(n1, n2);
+
+    open_fs("foo.txt");
+    // lseek(fd, BLOCK_SIZE, SEEK_SET);
+    // read(fd, &temp, sizeof(temp));
+
+    // printf("nfree value is %d \n", temp.nfree);
+
 }
 
